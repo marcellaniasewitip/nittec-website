cb510d7f2cd8e16effa01492f09eb6a4b8b4f29d
Marcel <marcellancewitip15@gmail.com>
2026-03-03 16:45:48 +1000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+// Inside src/main.tsx or your main entry file
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App' // or Index
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* ADD THE BASENAME HERE */}
+    <BrowserRouter basename="/nittec-website">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
